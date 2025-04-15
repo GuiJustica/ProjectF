3a43bad933bbdd596ac421d43901a807c5aabc89
Mr.Felix <104478664+ChuckFelix765@users.noreply.github.com>
2025-04-15 10:19:59 -0300

    Menu
    Fiz um proto menu e com alguns botões, depois eu ajeito melhor

---
+++ ProjectF/Assets/scripts/Menu/Selecao.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour{
+    public void inicio (){
+        SceneManager.LoadScene("CenaT");
+    }
+    public void reset(){
+        SceneManager.LoadScene("Menu");
+    }
+    public void sair(){
+        Debug.Log("Sair!");
+        Application.Quit();
+    }
+}
