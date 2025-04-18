3f3a918b7550ad4faf9bc2b8f13b8de37c749ac5
jvgoverna <jv11governa2018@gmail.com>
2025-04-18 14:59:11 -0300

    ADD Texto com o número de antenas levantadas

--- ProjectF/Assets/scripts/Kwid/LevantarAntena.cs
+++ ProjectF/Assets/scripts/Kwid/LevantarAntena.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LevantarAntena : MonoBehaviour{
 
@@ -8,21 +9,42 @@ public class LevantarAntena : MonoBehaviour{
     public SpriteRenderer initialImage;
     private bool changeImage = false;
 
+    public static int antennasRaised = 0;
+
     private bool cancelInteraction = false; // Variável para controlar a interação cancelada
     public GameObject interactionE; // Referência ao objeto de interação
 
+    public TextMeshProUGUI text;
+
+    private bool antennaAleradyRaised = false;
+
     void Start(){
         interactionE.SetActive(false); // Desativa o objeto de interação no início
+        text.gameObject.SetActive(false);
     }
 
     void Update(){
         if(Input.GetKeyDown(KeyCode.E) && changeImage){
             initialImage.sprite = imageWithRaiseAntenna;
             Debug.Log("Antena levantada!");
+            text.gameObject.SetActive(true);
+            Invoke("hideText", 2f); // Chama a função hideText após 2 segundos
             cancelInteraction = true; // Define a interação como cancelada
+
+            if(!antennaAleradyRaised){
+                antennasRaised++;
+                antennaAleradyRaised = true; // Define que a antena já foi levantada
+            }
+            text.text = "Antenas do kwid levantadas: " + antennasRaised.ToString() + "/15";
         }
 
     }
+
+    void hideText(){
+        text.gameObject.SetActive(false);
+    }
+    
+
 
     void OnTriggerEnter2D(Collider2D collision){
         if (collision.CompareTag("Player") && !cancelInteraction){
