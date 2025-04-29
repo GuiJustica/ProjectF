701323600971401fdb0d1518264ca593393ce264
PauloViniciudAF <pauloviniciuspvaf@gmail.com>
2025-04-29 17:57:24 -0300

    teste

--- ProjectF/Assets/scripts/Kwid/KwidScript.cs
+++ ProjectF/Assets/scripts/Kwid/KwidScript.cs
@@ -8,11 +8,18 @@ public class KwidScript : MonoBehaviour, IInteractable
    public bool TaLevantada { get; private set; }
    public string KwidID {get; private set ; }
    public Sprite levantadoSprite;
+   public Sprite initialImage;
+   private bool cancelInteraction = false;
 
     // Start is called before the first frame update
     void Start()
     {
         KwidID ??= GlobalHelper.GenerateUniqueID(gameObject); 
+        if (PlayerPrefs.GetInt(KwidID, 0) == 1){ // Verifica se a antena já foi levantada
+            initialImage = levantadoSprite;
+            cancelInteraction = true;
+            TaLevantada = true;
+        }
     }
 
     public void Interact()
