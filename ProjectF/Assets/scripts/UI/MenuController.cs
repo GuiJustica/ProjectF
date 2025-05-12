4f4a555fbc4b6f19b5e93a884c86871d5576766e
PauloViniciudAF <pauloviniciuspvaf@gmail.com>
2025-05-12 05:19:05 -0300

    arrumando tudo

--- ProjectF/Assets/scripts/UI/MenuController.cs
+++ ProjectF/Assets/scripts/UI/MenuController.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
     public GameObject menuCanvas;
+    public TextMeshProUGUI DescricaoTxt;
+    GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
         menuCanvas.SetActive(false);
+        gameManager = GameManager.Instance;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             if (!menuCanvas.activeSelf && PauseController.JogoPausado)
@@ -25,5 +30,6 @@ public class MenuController : MonoBehaviour
             PauseController.SetPause(menuCanvas.activeSelf);
 
         }
+        DescricaoTxt.text = LevantarAntena.antennasRaised.ToString() + "/?";
     }
 }
